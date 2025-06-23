20cca8b2548447bc32c8e9e264691f20d9e4435a
satyam7803 <satyam7803@gmail.com>
2025-06-23 14:48:46 +0530

    mogodb

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const express = require('express');  // Expres module include
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const authRoutes = require('./src/routes/authRoutes')
+const mongoose = require('mongoose');
 
 const app = express(); // instance of express application
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("database connected");
+})
+.catch((error)=>{
+    console.log(error);
+})
 
 app.use(express.json());  // Middleware to convert req into json format
 app.use(cookieParser());
--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,18 +1,31 @@
 const { request } = require('express');
 const jwt = require('jsonwebtoken');
 const secret = "6aaae49a-1904-4b23-94c0-21f868d3f0ad";
+const Users = require('../model/User')
+const bcrypt = require('bcryptjs')
 
 const authController = {
-    login : (request, response) => {
+    login : async (request, response) => {
         //these values are here beacause of express.json() middleware;
-        const {username, password} = request.body;
 
-        if(username === 'admin' && password === 'admin'){
-            const userDetails = {
-                name : "John Cena",
-                email : "John@email.com"
+        try{
+            const {username, password} = request.body;
+
+            const data = await Users.findOne({email: username})
+            if(!data){
+                return response.status(401).json({message: 'Invalid Credentials'});
             }
 
+            const isMatch = await bcrypt.compare(password, data.password)
+            if(!isMatch){
+                return response.status(401).json({message: 'Invalid'});
+            }
+
+            const userDetails = {
+                id: data._id,
+                name: data.name,
+                email: data.email
+            }
             const token = jwt.sign(userDetails, secret, {expiresIn: '1h'});
 
             response.cookie('jwtToken', token,{
@@ -21,12 +34,27 @@ const authController = {
                 domain : 'localhost',
                 path : '/'
             })
-
             response.status(200).json({message:'User Authenticated', userDetails:userDetails})
+
+            
+            // if(username === 'admin' && password === 'admin'){
+            //     const userDetails = {
+            //         name : "John Cena",
+            //         email : "John@email.com"
+            //     }
+    
+    
+    
+            // }
+            // else{
+            //     response.status(401).json({message : 'Invalid credentials'});
+            // }
+
+        } catch(error){
+            console.log(error);
+            response.status(404).json({message : "Invalid"});
         }
-        else{
-            response.status(401).json({message : 'Invalid credentials'});
-        }
+
     },
     logout : (request,response) => {
         response.clearCookie('jwtToken');
